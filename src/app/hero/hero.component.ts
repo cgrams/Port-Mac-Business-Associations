b3749b3cd2f8ced8961dcee0eb9c059783b833e0
Caleb Grams <cmgrams@gmail.com>
2019-07-19 11:44:06 +1000

    m

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ParallaxModule, ParallaxConfig } from 'ngx-parallax';
 
 @Component({
@@ -8,9 +8,18 @@ import { ParallaxModule, ParallaxConfig } from 'ngx-parallax';
 })
 export class HeroComponent implements OnInit {
 
-  constructor() { }
+  constructor(){}
 
-  ngOnInit() {
-  }
+  ngOnInit(){}
+	
+	cardColor:any = 1;
+	scrollPositionNew:any;
+	transformSwing:any = {first:"30%", second: "20%"};
+	@HostListener('window:scroll', ['$event']) 
+	    doSomething(event) {
+	    	const scrollPosition = window.pageYOffset;
+	    	this.scrollPositionNew = scrollPosition/100;
+	    	if(scrollPosition>100){ this.cardColor= this.scrollPositionNew ;console.log("cooldddd"); this.transformSwing.first = scrollPosition/3+"%";}
+	    }
 
 }
